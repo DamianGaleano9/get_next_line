fabc67a08c39af268851139b973cbb99bc7e5bf1
DamianGaleano9 <dj.damian.b@hotmail.com>
2024-12-17 19:02:10 +0100

    Initial

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmazo-ga <dmazo-ga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 17:51:51 by dmazo-ga          #+#    #+#             */
+/*   Updated: 2024/12/17 18:10:24 by dmazo-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET _NEXT_LINE_H
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifndef BUFFER_SIZE
+#	define BUFFER_SIZE 8
+#endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ftstrdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
